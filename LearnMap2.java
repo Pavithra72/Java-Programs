3e456a4cdede4b81627572b478d82a127c28b4ed
Pavithra72 <63652428+Pavithra72@users.noreply.github.com>
2020-05-21 19:59:21 +0530

    Add files via upload

---
+++ LearnMap2.java
@@ -0,0 +1,27 @@
+package Java_Session;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class LearnMap2 {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * 1. Write a Java Program to print the keys and values from the Map map = {A=1,
+		 * B=2, C=3, D=4, E=5} (Min 3 ways) Note: Even if you google understand what you
+		 * are doing! Output A->1 B->2 C->3 D->4 E->5
+		 */
+		 Map<String,Integer> map = new LinkedHashMap<String,Integer>();
+		 map.put("A", 1);
+		 map.put("B", 2);
+		 map.put("C", 3);
+		 map.put("D", 4);
+		 map.put("E", 5);
+
+		 map.forEach((eachKey,eachValue)->System.out.println(eachKey+"-> "+eachValue));
+		 
+	}
+
+}
---
+++ LearnMap3.java
@@ -0,0 +1,26 @@
+package Java_Session;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class LearnMap3 {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * 1. Write a Java Program to print the keys and values from the Map map = {A=1,
+		 * B=2, C=3, D=4, E=5} (Min 3 ways) Note: Even if you google understand what you
+		 * are doing! Output A->1 B->2 C->3 D->4 E->5
+		 */
+		 Map<String,Integer> map = new LinkedHashMap<String,Integer>();
+		 map.put("A", 1);
+		 map.put("B", 2);
+		 map.put("C", 3);
+		 map.put("D", 4);
+		 map.put("E", 5);
+
+		map.keySet().forEach((eachKey)->System.out.println(eachKey+"-> "+map.get(eachKey)));		 
+	}
+
+}
